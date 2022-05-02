b0dabb08bd9eac7d94d8af51a4cfcfc3263dc0a7
TheTiagoMarabuto <up201604598@fe.up.pt>
2022-05-02 18:07:24 +0100

    new version

--- test.py
+++ test.py
@@ -1,105 +1,12 @@
-import os
-import sys
+import dearpygui.dearpygui as dpg
+import dearpygui.demo as demo
 
-import glfw
-import OpenGL.GL as gl
-import imgui
-from imgui.integrations.glfw import GlfwRenderer
+dpg.create_context()
+dpg.create_viewport(title='Custom Title', width=600, height=600)
 
-active = {"window": True, "child": False, "tooltip": False, "menu bar": False, "popup": False, "popup modal": False,
-    "popup context item": False, "popup context window": False, "drag drop": False, "group": False, "tab bar": False,
-    "list box": False, "popup context void": False, "table": False, }
-path_to_font = None  # "path/to/font.ttf"
+demo.show_demo()
 
-opened_state = True
-
-
-# Frame commands from the video
-def frame_commands():
-    io = imgui.get_io()
-    if io.key_ctrl and io.keys_down[glfw.KEY_Q]:
-        sys.exit(0)
-
-    if imgui.begin_main_menu_bar():
-        if imgui.begin_menu("File"):
-            clicked, selected = imgui.menu_item("Quit", "Ctrl+Q")
-            if clicked:
-                sys.exit(0)
-            imgui.end_menu()
-        imgui.end_main_menu_bar()
-
-    with imgui.begin("A Window!"):
-        if imgui.button("select"):
-            imgui.open_popup("select-popup")
-
-        try:
-            with imgui.begin_popup("select-popup") as popup:
-                if popup.opened:
-                    imgui.text("Select one")
-                    raise Exception
-        except Exception:
-            print("caught exception and no crash!")
-
-
-def render_frame(impl, window, font):
-    glfw.poll_events()
-    impl.process_inputs()
-    imgui.new_frame()
-
-    gl.glClearColor(0.1, 0.1, 0.1, 1)
-    gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-
-    if font is not None:
-        imgui.push_font(font)
-    frame_commands()
-    if font is not None:
-        imgui.pop_font()
-
-    imgui.render()
-    impl.render(imgui.get_draw_data())
-    glfw.swap_buffers(window)
-
-
-def impl_glfw_init():
-    width, height = 1600, 900
-    window_name = "minimal ImGui/GLFW3 example"
-
-    if not glfw.init():
-        print("Could not initialize OpenGL context")
-        sys.exit(1)
-
-    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
-    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-    glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
-
-    window = glfw.create_window(int(width), int(height), window_name, None, None)
-    glfw.make_context_current(window)
-
-    if not window:
-        glfw.terminate()
-        print("Could not initialize Window")
-        sys.exit(1)
-
-    return window
-
-
-def main():
-    imgui.create_context()
-    window = impl_glfw_init()
-
-    impl = GlfwRenderer(window)
-
-    io = imgui.get_io()
-    jb = io.fonts.add_font_from_file_ttf(path_to_font, 30) if path_to_font is not None else None
-    impl.refresh_font_texture()
-
-    while not glfw.window_should_close(window):
-        render_frame(impl, window, jb)
-
-    impl.shutdown()
-    glfw.terminate()
-
-
-if __name__ == "__main__":
-    main()
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
--- test2.py
+++ test2.py
@@ -1,8 +1,57 @@
 import dearpygui.dearpygui as dpg
 
+from tools import *
+from dijkstra import *
+
+graph= build_graph(read_json("/Users/tiagomarabuto/PycharmProjects/SAD3E_v1/edges.json"))
+exit_array = ["U", "S"]
+set_nearest_exit(graph, exit_array)
+WHITE = (255, 255, 255)
+ORANGE = (255, 140, 0)
+GOLD = (255, 215, 0)
+seen_nodes = []
+node_label = defaultdict()
+edge_label = defaultdict()
 dpg.create_context()
-dpg.create_viewport(title="SAD3E")
-dpg.show_documentation()
+
+# callback runs when user attempts to connect attributes
+def link_callback(sender, app_data):
+    # app_data -> (link_id1, link_id2)
+    dpg.add_node_link(app_data[0], app_data[1], parent=sender)
+
+# callback runs when user attempts to disconnect attributes
+def delink_callback(sender, app_data):
+    # app_data -> link_id
+    dpg.delete_item(app_data)
+
+with dpg.window(label="Tutorial", width=400, height=400):
+
+    with dpg.node_editor(label="parent", callback=link_callback, delink_callback=delink_callback):
+        for node in graph:
+            seen_nodes.append(node)
+            for dst, weight, hazard in graph[node].edges:
+                # Check if edge was already drew
+                if dst not in seen_nodes:
+                    a=5
+                    #edge_label[(node, dst)] = dpg.draw_line((30, 30), (50,50), label="HELLO", parent="parent")
+                        #(graph[node].location[0], graph[node].location[1]),
+                        #(graph[dst].location[0], graph[dst].location[1]), color=GOLD)
+
+
+            # First draw edges and then the nodes
+            with dpg.node(label=node, pos=(graph[node].location[0]*5,600-graph[node].location[1]*3),
+                          draggable=False):
+
+                dpg.add_node_attribute(label="in", shape=dpg.mvNode_PinShape_Triangle)
+
+                with dpg.node_attribute(label="out", attribute_type=dpg.mvNode_Attr_Output,
+                                        shape=dpg.mvNode_PinShape_Triangle):
+                    dpg.add_text("Next:" + graph[node].next_node)
+
+            #dpg.add_node_link(26,24)
+
+
+dpg.create_viewport(title='Custom Title', width=800, height=600)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
